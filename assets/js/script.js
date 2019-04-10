4e9108243a6593d3f6e47771391bdf43bd16b8cb
Soul <Soul@ZENBOOK>
2019-04-10 16:33:38 +0400

    restart

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,28 @@ var rand;
 var sum = 0
 var win = 0
 var losses = 0
-var mycaracterhelth = Math.floor(Math.random() * 60) + 120;
+var mycharacterhelth = Math.floor(Math.random() * 60) + 120;
 var defenderhelth = Math.floor(Math.random() * 60) + 120;
 selected_image = ""
 select_attacked = true
 select_defender = false
 
-var images = $("img")
+var imgPath=["./assets/images/darth-sidious.png","./assets/images/luke-skywalker.jpg","./assets/images/obi-wan.jpg","./assets/images/darth-maul.jpg"]
 
-$("img").on("click", function () {
+function load(){
+    for(let i=0;i<4;i++){
+        let images = $("<img>")
+        $(images).attr('src',imgPath[i])
+        $("#selected-character-section").append(images)
+}
+}
+var images=[]
+$(document).ready(function(){
+    load()
+    images=$("img")
+})
+
+$(document).on("click","img", function () {
     selected_image = $(this).get(0)
     
     if (select_attacked) {
@@ -24,7 +37,7 @@ $("img").on("click", function () {
                 $("#selected-character").append(selected_image)
                 select_attacked = false
                 select_defender = true
-                $("#helth-selected").html("Your Helth : " +  mycaracterhelth)
+                $("#helth-selected").html("Your Helth : " +  mycharacterhelth)
             }
         }
     }
@@ -46,22 +59,62 @@ function get_fighter() {
     select_defender = true
 }
 
+function reStart(){
+    $(".re-start").hide();
+    
+    sum=0;
+    win=0;
+    losses=0;
+    
+    $("#selected-character-section").empty()
+    $("#selected-character").empty();
+    $("#helth-selected").html("")
+    $("#defender").empty();
+    $(".caracter").empty();
+
+
+}
+
+$(".re-start").on("click",function(){
+    reStart();
+    // <div id="selected-character"></div>
+
+	// 		<h4 id="helth-selected"></h4>
+    let div=$("<div>");
+    div.attr("id","selected-character");
+    let h4=$("<h4>");
+h4.attr("id","helth-selected");
+    $("#selected-character-section").append(h4)
+    $("#selected-character-section").append(div);
+
+    load()
+})
+
 $("#attack-button").on("click",function(){
-    let attack = Math.floor(Math.random() * 15) + 25 + 30;
-    let defend = Math.floor(Math.random() * 15) + 25;
-    mycaracterhelth -= defend
-    defenderhelth -= attack 
-    $("#chat").html("your attack :" + attack + "\n" + "defender attack : " + defend)
-    if (mycaracterhelth <= 0) {
-        alert("Defender is win! you loses!")
-    }else if (defenderhelth <= 0 ){
-        alert("You Win! defender loses")
-        get_fighter()
-        $("#defender").empty()
-        defenderhelth =  Math.floor(Math.random() * 60) + 120;;
+    //mycharacterhealth
+    if(mycharacterhelth>0){
+
+        let attack = Math.floor(Math.random() * 15) + 25 + 30;
+        let defend = Math.floor(Math.random() * 15) + 25;
+        mycharacterhelth -= defend
+        defenderhelth -= attack 
+        $("#chat").html("your attack :" + attack + "\n" + "defender attack : " + defend)
+        if (mycharacterhelth <= 0) {
+            alert("Defender is win! you loses!")
+            
+        }
+        else if (defenderhelth <= 0 ){
+            alert("You Win! defender loses")
+            get_fighter()
+            $("#defender").empty()
+            defenderhelth =  Math.floor(Math.random() * 60) + 120;
+        }
+        $("#helth-selected").html("Your Helth : " +  mycharacterhelth)
+        $("#helth-defender").html("defender health : " + defenderhelth)
+    }else {
+        $(".re-start").show();
+        
     }
-    $("#helth-selected").html("Your Helth : " +  mycaracterhelth)
-    $("#helth-defender").html("defender health : " + defenderhelth)
   
 })
 
